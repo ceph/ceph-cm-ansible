e20916d9430d7bed38568ee45d5cc9937d96ca1e
David Galloway <dgallowa@redhat.com>
2018-11-16 15:14:44 -0500

    cobbler: Configure snippets & triggers for RHEL8
    Signed-off-by: David Galloway <dgallowa@redhat.com>

--- roles/cobbler/templates/triggers/install/post/cephlab_ansible.sh
+++ roles/cobbler/templates/triggers/install/post/cephlab_ansible.sh
@@ -26,8 +26,18 @@ pushd $ANSIBLE_CM_PATH
 flock --close ./.lock git pull
 export ANSIBLE_SSH_PIPELINING=1
 export ANSIBLE_HOST_KEY_CHECKING=False
+
+# Set ansible_python_interpeter var if RHEL8
+# https://docs.ansible.com/ansible/2.7/reference_appendices/python_3_support.html
+if [[ $(cobbler system dumpvars --name $2 | grep os_version | awk '{ print $3 }') == "rhel8" ]]; then
+  # Nagios packages aren't available in the RHEL8 beta image so we'll skip those tasks
+  ANSIBLE_EXTRAVAR="-e ansible_python_interpreter=/usr/bin/python3 --skip-tags nagios"
+else
+  ANSIBLE_EXTRAVAR=""
+fi
+
 # Tell ansible to create users, populate authorized_keys, and zap non-root disks
-ansible-playbook testnodes.yml -v --limit $name* --tags user,pubkeys,zap 2>&1 > /var/log/ansible/$name.log
+ansible-playbook $ANSIBLE_EXTRAVAR testnodes.yml -v --limit $name* --tags user,pubkeys,zap 2>&1 > /var/log/ansible/$name.log
 # Now run the rest of the playbook. If it fails, at least we have access.
 # Background it so that the request doesn't block for this part and end up 
 # causing the client to retry, thus spawning this trigger multiple times
@@ -37,5 +47,5 @@ if [[ $profile == *"-stock" ]]
 then
     exit 0
 fi
-ansible-playbook testnodes.yml -v --limit $name* --skip-tags user,pubkeys,zap 2>&1 >> /var/log/ansible/$name.log &
+ansible-playbook $ANSIBLE_EXTRAVAR testnodes.yml -v --limit $name* --skip-tags user,pubkeys,zap 2>&1 >> /var/log/ansible/$name.log &
 popd
